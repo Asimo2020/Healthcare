c2671861cafcf248e73f8099fb38fabfbcbb6ca9
Asimo2020 <ghahramani89@gmail.com>
2024-10-01 00:38:13 -0700

    create views for apis

--- healthcare_appointment_system/api/views.py
+++ healthcare_appointment_system/api/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets, permissions
+from .models import Doctor, Patient, Appointment, MedicalRecord
+from .serializers import DoctorSerializer, PatientSerializer, AppointmentSerializer, MedicalRecordSerializer
+
+class DoctorViewSet(viewsets.ModelViewSet):
+    queryset = Doctor.objects.all()
+    serializer_class = DoctorSerializer
+    permission_classes = [permissions.IsAdminUser]
+
+class PatientViewSet(viewsets.ModelViewSet):
+    queryset = Patient.objects.all()
+    serializer_class = PatientSerializer
+
+class AppointmentViewSet(viewsets.ModelViewSet):
+    queryset = Appointment.objects.all()
+    serializer_class = AppointmentSerializer
+
+class MedicalRecordViewSet(viewsets.ModelViewSet):
+    queryset = MedicalRecord.objects.all()
+    serializer_class = MedicalRecordSerializer
