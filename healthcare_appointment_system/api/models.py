9a50c843f09db2fd62dbb3d2efd5fe1401cb05bb
Asimo2020 <ghahramani89@gmail.com>
2024-10-13 09:16:52 +0330

    AppointmentReport

--- healthcare_appointment_system/api/models.py
+++ healthcare_appointment_system/api/models.py
@@ -1,21 +1,21 @@
 
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
 
-class User(AbstractUser):
-    ROLE_CHOICES = (
-        ('admin','Admin'),
-        ('doctor','Doctor'),
-        ('patient', 'Patient'),
-    )
-    role = models.CharField(max_length=10, choices= ROLE_CHOICES)
+# class User(AbstractUser):
+#     ROLE_CHOICES = (
+#         ('admin','Admin'),
+#         ('doctor','Doctor'),
+#         ('patient', 'Patient'),
+#     )
+    # role = models.CharField(max_length=10, choices= ROLE_CHOICES)
 class Doctor(models.Model):
-    user = models.OneToOneField('api.User', on_delete=models.CASCADE)
+    user = models.OneToOneField('User', on_delete=models.CASCADE)
     specialization = models.CharField(max_length=100)
-    availability = models.JSONField()  # Example: [{"day": "Monday", "slots": ["09:00", "10:00"]}, ...]
+    availability = models.JSONField() 
 
 class Patient(models.Model):
-    user = models.OneToOneField('api.User', on_delete=models.CASCADE)
+    user = models.OneToOneField('User', on_delete=models.CASCADE)
     phone_number = models.CharField(max_length=15)
     medical_history = models.TextField()
 
@@ -23,13 +23,14 @@ class Appointment(models.Model):
     doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
     patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
     appointment_time = models.DateTimeField()
-    status = models.CharField(max_length=20, default='Scheduled')
-    choices=[('scheduled','Scheduled'),
-             ('completed', 'Completed')]
+    # status = models.CharField(max_length=20, default='Scheduled')
+    # choices=[('scheduled','Scheduled'),
+    #          ('completed', 'Completed')]
 
 class MedicalRecord(models.Model):
     patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
-    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    # doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    record_details = models.TextField()
     notes = models.TextField()
     date_created = models.DateTimeField(auto_now_add=True)
 
@@ -43,4 +44,12 @@ class Bill(models.Model):
     Patient = models.ForeignKey(Patient, on_delete= models.CASCADE)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     paid = models.BooleanField(default=False)
+    status = models.CharField(max_length=10, choices=[('paid', 'Paid'), ('pending', 'Pending')])
+
+
+class AppointmentReport(models.Model):
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    appointment_date = models.DateTimeField()
+    notes = models.TextField()
 
--- healthcare_appointment_system/api/views.py
+++ healthcare_appointment_system/api/views.py
@@ -6,19 +6,19 @@ from rest_framework.permissions import IsAuthenticated
 from .models import Doctor, Patient, Appointment, MedicalRecord
 from .serializers import DoctorSerializer, PatientSerializer, AppointmentSerializer, MedicalRecordSerializer
 import logging
-
+from .Permissions import *
 logger = logging.getLogger(__name__)
 class DoctorViewSet(viewsets.ModelViewSet):
     logger.error("hello")
     queryset = Doctor.objects.all()
 
     serializer_class = DoctorSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsDoctor]
                      
 class PatientViewSet(viewsets.ModelViewSet):
     queryset = Patient.objects.all()
     serializer_class = PatientSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsPatient]
 
 class AppointmentViewSet(viewsets.ModelViewSet):
     queryset = Appointment.objects.all()
@@ -29,3 +29,7 @@ class MedicalRecordViewSet(viewsets.ModelViewSet):
     queryset = MedicalRecord.objects.all()
     serializer_class = MedicalRecordSerializer
     permission_classes = [IsAuthenticated]
+class PerscriptionViewSet(viewsets.ModelViewSet):
+    queryset = Perscription.objects.all()
+    serializer_class = PerscriptionSerializer
+    permission_classes = [permissions.IsAuthenticated]
