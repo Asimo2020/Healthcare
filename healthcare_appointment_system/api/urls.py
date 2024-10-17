bb9f3442818d53872efa9587451112ddf8735721
Asimo2020 <ghahramani89@gmail.com>
2024-10-17 21:34:34 +0330

    addurl:PrescriptionUrl

--- healthcare_appointment_system/api/urls.py
+++ healthcare_appointment_system/api/urls.py
@@ -1,14 +1,16 @@
 
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import DoctorViewSet, PatientViewSet, AppointmentViewSet, MedicalRecordViewSet, BillViewSet
+from .views import DoctorViewSet, PatientViewSet, AppointmentViewSet, MedicalRecordViewSet, BillViewSet,PrescriptionViewSet
 
 router = DefaultRouter()
 router.register(r'doctors', DoctorViewSet)
 router.register(r'patients', PatientViewSet)
 router.register(r'appointments', AppointmentViewSet)
 router.register(r'medical-records', MedicalRecordViewSet)
-router.register(r'Bill', BillViewSet)
+router.register(r'Bill', BillViewSet)#
+router.register(r'Prescription',PrescriptionViewSet )
+
 
 urlpatterns = [
     path('', include(router.urls)),
